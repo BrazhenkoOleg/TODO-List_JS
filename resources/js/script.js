b2757ef98c4131a57dd9a5dc46410ad4b0f15d66
O-Brazhenko <o.brazhenko@tg.dunice.net>
2024-07-24 18:10:52 +0300

    Added delete All Checked, checked All

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,13 +1,16 @@
 const taskInput = document.querySelector('#task-input');
 const taskContainer = document.querySelector('#task-container');
 const addTaskButton = document.querySelector('#task-button');
+const checkAll = document.querySelector('#check-all');
+const deleteAllCheck = document.querySelector('#delete-all-check');
 
 let taskList = [];
+let currentPage = 2;
 
 const listenEvent = (e) => {
     const finderTask = taskList.find((task) => task._id == e.target.parentElement.id);
-    console.log(e);
-    switch (e.target.getAttribute('name')) {
+
+    /*switch (e.target.getAttribute('name')) {
         case 'check': 
             console.log('Check');
             checkTask(finderTask);
@@ -19,39 +22,25 @@ const listenEvent = (e) => {
         case 'text':
             if (e.detail == 2) {
                 const textTask = e.target;
-                console.log(e.target.parentElement);
-                const inputTask = e.target.parentElement.querySelector('input[name="edit"]');
-                textTask.hidden = !textTask.hidden;
-                inputTask.hidden = !inputTask.hidden;
-                console.log("span: ", textTask);
-                console.log("input: ", inputTask);
+                const inputTask = e.target.nextElementSibling;
+                inputTask.hidden = false;
+                textTask.hidden = true;
+                console.log("span: \n", textTask);
+                console.log("input: \n", inputTask);
                 inputTask.value = textTask.textContent;
                 inputTask.focus();
-                
-                inputTask.onkeydown = (e) => {
-                    if (e.key === 'Enter') {
-                        e.preventDefault();
-                        textTask.hidden = !textTask.hidden;
-                        inputTask.hidden = !inputTask.hidden;
-                        textTask.textContent = inputTask.value;
-                        editTask(finderTask, inputTask.value);
-                    }
-                };
-                
-                inputTask.onblur = (e) => {
-                    textTask.hidden = !textTask.hidden;
-                    inputTask.hidden = !inputTask.hidden;
-                    textTask.textContent = inputTask.value;
-                    editTask(finderTask, inputTask.value);
-                }
-                //textTask.textContent = inputTask.value;
-                //editTask(finderTask, inputTask.value);
             }
             break;
-    }
+    }*/
+
+    if (e.target.getAttribute('name') === 'check') checkTask(finderTask);
+
+    if (e.target.getAttribute('name') === 'delete') deleteTask(finderTask);
+
+    if (e.target.getAttribute('name') === 'text' && e.detail === 2) findTaskEdit(e.target);
 }
 
-addTask = () => {
+const addTask = () => {
     const taskInfo = {
         _id: Date.now(),
         text: taskInput.value,
@@ -65,8 +54,22 @@ addTask = () => {
 
 const checkTask = (task) => {
     task.isChecked = !task.isChecked;
-    console.log(taskList);
+    //console.log(taskList);
     render(); 
+}
+
+const checkAllTask = (check) => {
+    taskList.forEach((task) => task.isChecked = check);
+    render();
+}
+
+const findTaskEdit = (target) => {
+    const textTask = target;
+    const inputTask = target.nextElementSibling;
+    inputTask.hidden = false;
+    textTask.hidden = true;
+    inputTask.value = textTask.textContent;
+    inputTask.focus();
 }
 
 const editTask = (task, text) => {
@@ -79,28 +82,99 @@ const deleteTask = (task) => {
     render(); 
 } 
 
-    taskInput.onkeydown = (e) => {
-        if (e.key === 'Enter') {
-            e.preventDefault();
-            addTask();
-        }
-    };
+const deleteCheckTask = () => {
+    taskList = taskList.filter((tasks) => !tasks.isChecked);
+
+    render();
+}
 
 const getLayoutTodo = (task) => {
     return taskContainer.innerHTML = 
-        `<li id='${task._id}' class='list-group-item'>
+        `<li id='${task._id}' class='list-group-item list-group-item-action'>
             <input name='check' class='form-check-input' type='checkbox' ${task.isChecked ? 'checked' : ''} />
             <span name='text' class='form-check-label' > ${task.text} </span>
-            <input name='edit' type='text' class='form-control me-2' hidden />
+            <input name='edit' type='text' class='form-control' value='' hidden />
             <button name='delete' type='button' class='btn btn-danger'> Delete </button>
         </li>`;
 }
 
+const paginationTask = (page) => {
+    const countElem = 5;
+    //const list = taskList.filter((task, index) => );
+    console.log(list);
+}
+
 const render = () => {
     taskContainer.innerHTML = '';
-    taskList.forEach((task) => taskContainer.innerHTML += getLayoutTodo(task));
-};
+    taskList.forEach((task) => {
+        taskContainer.innerHTML += getLayoutTodo(task);
+    });
 
+    if (taskList.length > 0) {
+        checkAll.disabled = false;
+        checkAll.checked = taskList.every((task) => task.isChecked === true);
+    } else {
+        checkAll.disabled = true;
+        checkAll.checked = false;
+    }
+
+    deleteAllCheck.disabled = !taskList.some((task) => task.isChecked === true);
+
+    console.log(checkAll.checked);
+    //paginationTask(currentPage);
+}
+
+const addTaskEvent = (e) => {
+    if (e.key === 'Enter') {
+        e.preventDefault();
+        addTask();
+    }
+}
+
+const keyTaskEvent = (e) => {
+    const finderTask = taskList.find((task) => task._id == e.target.parentElement.id);
+    const inputTask = e.target;
+    const textTask = e.target.previousElementSibling;
+    if (e.sourceCapabilities !== null && e.key === 'Enter') {
+        //console.log(e);
+        e.preventDefault();
+        textTask.hidden = false;
+        inputTask.hidden = true;
+        textTask.textContent = inputTask.value;
+        editTask(finderTask, inputTask.value);
+    }
+}
+
+const blurTaskEvent = (e) => {
+    const finderTask = taskList.find((task) => task._id == e.target.parentElement.id);
+    const inputTask = e.target;
+    const textTask = e.target.previousElementSibling;
+    if (e.sourceCapabilities !== null){
+        textTask.hidden = false;
+        inputTask.hidden = true;
+        textTask.textContent = inputTask.value;
+        editTask(finderTask, inputTask.value);
+    }
+}
+
+const checkAllEvent = (e) => {
+    const checkAll = e.target;
+    if (checkAll.id === 'check-all') {
+        checkAllTask(checkAll.checked);
+    }
+}
+
+const deleteAllCheckEvent = (e) => {
+    const deleteAllCheck = e.target;
+    if (deleteAllCheck.id === 'delete-all-check') { 
+        deleteCheckTask();
+    }
+}
 
 addTaskButton.addEventListener('click', addTask);
+taskInput.addEventListener('keydown', addTaskEvent);
+checkAll.addEventListener('click', checkAllEvent);
+deleteAllCheck.addEventListener('click', deleteAllCheckEvent);
 taskContainer.addEventListener('click', listenEvent);
+taskContainer.addEventListener('keydown', keyTaskEvent);
+taskContainer.addEventListener('blur', blurTaskEvent, true);
