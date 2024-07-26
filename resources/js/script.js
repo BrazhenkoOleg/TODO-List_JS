96f72e81609582aace9976e43ca0d940c5def6f5
O-Brazhenko <o.brazhenko@tg.dunice.net>
2024-07-26 17:59:53 +0300

    Added filter tasks

--- resources/js/script.js
+++ resources/js/script.js
@@ -4,11 +4,18 @@ const addTaskButton = document.querySelector('#task-button');
 const checkAll = document.querySelector('#check-all');
 const deleteAllCheck = document.querySelector('#delete-all-check');
 const pageContainer = document.querySelector('#pages');
+const filterContainer = document.querySelector('#filter');
 
 const COUNT_ELEM = 5;
+const FILTER_TASKS = {
+    all : 'All',
+    active : 'Active',
+    complited : 'Complited'
+};
 
 let taskList = [];
 let currentPage = 1;
+let filterType = 'all';
 
 const listenEvent = (e) => {
     const finderTask = taskList.find((task) => task._id == e.target.parentElement.id);
@@ -44,12 +51,15 @@ const listenEvent = (e) => {
 }
 
 const addTask = () => {
+    //validation task
+
     const taskInfo = {
         _id: Date.now(),
         text: taskInput.value,
         isChecked: false
     };
 
+    filterType = 'all';
     getLayoutTodo(taskInfo);
     taskList.push(taskInfo); 
     currentPageController('add');
@@ -74,6 +84,7 @@ const findTaskEdit = (target) => {
     textTask.hidden = true;
     inputTask.value = textTask.textContent;
     inputTask.focus();
+    checkInputTask(inputTask.value);
 }
 
 const editTask = (task, text) => {
@@ -93,6 +104,34 @@ const deleteCheckTask = () => {
     render();
 }
 
+
+const checkInputTask = (input) => {
+    if (!(/^\S{3,}$/.test(input))) {
+        return false;
+    } 
+    else {
+        return true;
+    }
+}
+
+const renderLayoutFilter = () => {
+    //TODO: delete _ ? _ : _
+    filterContainer.innerHTML = '';
+    for (const [id, text] of Object.entries(FILTER_TASKS)){
+        filterContainer.innerHTML += 
+            `<button id=${id} type="button" class="btn ${id === 'all' ? 
+                'btn-danger' : id === 'active' ? 
+                'btn-warning' : id === 'complited' ? 
+                'btn-success' : '' } ${filterType === id ? 'active' : ''}">${text} <span class="badge text-bg-primary rounded-pill"> ${currentFilter(id).length} </span></button>`
+    }
+
+    return filterContainer.innerHTML;
+    //return filterContainer.innerHTML += 
+    //`<button id=${filter[0]} type="button" class="btn btn-danger ${filterType === filter[0] ? 'active' : ''}">${filter[1]} <span class="badge text-bg-primary rounded-pill"> ${taskList.length} </span></button>
+    //<button id=${filter[0]} type="button" class="btn btn-warning ${filterType === filter[0] ? 'active' : ''}">${filter[1]} <span class="badge text-bg-primary rounded-pill"> ${taskList.filter((task) => !task.isChecked).length} </span></button>
+    //<button id=${filter[0]} type="button" class="btn btn-success ${filterType === filter[0] ? 'active' : ''}">${filter[1]} <span class="badge text-bg-primary rounded-pill"> ${taskList.filter((task) => task.isChecked).length} </span></button>`;
+}
+
 const getLayoutTodo = (task) => {
     return taskContainer.innerHTML = 
         `<li id='${task._id}' class='list-group-item list-group-item-action'>
@@ -107,10 +146,36 @@ const getLayoutPage = (page) => {
     return pageContainer.innerHTML = `<li class="page-item ${page == currentPage ? 'active' : ''} "><a class="page-link" href="#"> ${page} </a></li>`;
 }
 
-const paginationTask = (page) => {
-    let sliceList = taskList.slice((page - 1) * COUNT_ELEM, page * COUNT_ELEM );
+const currentFilter = (type) => {
+    switch (type) {
+        case 'active':
+            return taskList.filter((task) => !task.isChecked);
+        case 'complited': 
+            return taskList.filter((task) => task.isChecked);
+        case 'all': 
+            return taskList;
+    }
+}
 
-    return sliceList;
+const filterTask = (type) => {
+    filterType = type;
+    return currentFilter(filterType);
+}
+
+const filterActive = () => {
+    let activeTask = taskList.filter((task) => task.isChecked === false);
+    return activeTask;
+}
+
+const filterComplited = () => {
+    let complitedTask = taskList.filter((task) => task.isChecked === true);
+    return complitedTask;
+}
+
+const paginationTask = () => {
+    let startIndex = (currentPage - 1) * COUNT_ELEM;
+    let endIndex = currentPage * COUNT_ELEM;
+    return filterTask(filterType).slice(startIndex, endIndex);
 }
 
 const currentPageController = (type) => {
@@ -127,25 +192,25 @@ const currentPageController = (type) => {
     }
 }
 
-const renderTasks = (page) => {
+const renderTasks = () => {
     taskContainer.innerHTML = '';
-    paginationTask(page).forEach((task) => taskContainer.innerHTML += getLayoutTodo(task));
+    paginationTask(filterTask(filterType)).forEach((task) => taskContainer.innerHTML += getLayoutTodo(task));
 }
 
 const renderActiveCheckAll = () => {
-    if (taskList.length > 0) {
+    if (filterTask(filterType).length > 0) {
         checkAll.disabled = false;
-        checkAll.checked = taskList.every((task) => task.isChecked === true);
+        checkAll.checked = filterTask(filterType).every((task) => task.isChecked === true);
     } else {
         checkAll.disabled = true;
         checkAll.checked = false;
     }
 }
 
-const renderActiveDeleteAll = () => deleteAllCheck.disabled = !taskList.some((task) => task.isChecked === true);
+const renderActiveDeleteAll = () => deleteAllCheck.disabled = !filterTask(filterType).some((task) => task.isChecked === true);
 
 const renderPagination = () => {
-    let countPage = Math.ceil(taskList.length / COUNT_ELEM);
+    let countPage = Math.ceil(filterTask(filterType).length / COUNT_ELEM);
     pageContainer.innerHTML = '';
     for (let i = 1; i <= countPage; i++) {
         if (countPage > 1) {
@@ -155,10 +220,12 @@ const renderPagination = () => {
 }
 
 const render = () => {
+    console.log(filterType);
+    renderLayoutFilter();
     renderPagination();
     renderActiveCheckAll();
     renderActiveDeleteAll();
-    renderTasks(currentPage);
+    renderTasks();
     /*const countPage = Math.floor((taskList.length + 1) / countElem);
     taskContainer.innerHTML = '';
 
@@ -186,7 +253,9 @@ const render = () => {
 const addTaskEvent = (e) => {
     if (e.key === 'Enter') {
         e.preventDefault();
-        addTask();
+        if (checkInputTask(e.target.value)) {
+            addTask();
+        }
     }
 }
 
@@ -199,6 +268,10 @@ const keyTaskEvent = (e) => {
         e.preventDefault();
         textTask.hidden = false;
         inputTask.hidden = true;
+        //if (checkInputTask(inputTask.value)) {
+        //    textTask.textContent = inputTask.value;
+        //    editTask(finderTask, inputTask.value);
+        //}
         textTask.textContent = inputTask.value;
         editTask(finderTask, inputTask.value);
     }
@@ -211,6 +284,11 @@ const blurTaskEvent = (e) => {
     if (e.sourceCapabilities !== null){
         textTask.hidden = false;
         inputTask.hidden = true;
+        //textTask.textContent = inputTask.value;
+        //if (checkInputTask(inputTask.value)) {
+        //    textTask.textContent = inputTask.value;
+        //    editTask(finderTask, inputTask.value);
+        //}
         textTask.textContent = inputTask.value;
         editTask(finderTask, inputTask.value);
     }
@@ -235,6 +313,14 @@ const movePageEvent = (e) => {
     render();
 }
 
+const filterEvent = (e) => {
+    const type = e.target.getAttribute('id');
+    currentPage = 1;
+    filterTask(type);
+    render(); 
+}
+
+document.addEventListener('DOMContentLoaded', renderLayoutFilter);
 addTaskButton.addEventListener('click', addTask);
 taskInput.addEventListener('keydown', addTaskEvent);
 checkAll.addEventListener('click', checkAllEvent);
@@ -243,3 +329,4 @@ taskContainer.addEventListener('click', listenEvent);
 taskContainer.addEventListener('keydown', keyTaskEvent);
 taskContainer.addEventListener('blur', blurTaskEvent, true);
 pageContainer.addEventListener('click', movePageEvent);
+filterContainer.addEventListener('click', filterEvent);
